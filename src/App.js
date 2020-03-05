1dc72f9baca5e4259c7114e2cafdd51ca87cf9d1
Siir <2485999859@qq.com>
2020-03-05 19:59:58 +0800

    submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SubComp extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUpdate: false
+    };
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    console.log(this.props.order + " ChildComponent WillRecieveProps");
+  }
+  UNSAFE_componentWillMount(){
+    console.log(this.props.order + " ChildComponent WillMounted");
+  }
+  componentDidMount() {
+    console.log(this.props.order + " ChildComponent Mounted");
+  }
+  UNSAFE_componentWillUpdate() {
+    console.log(this.props.order + " ChildComponent WillUpdate");
+  }
+  componentDidUpdate() {
+    console.log(this.props.order + " ChildComponent DidUpdate");
+  }
+
+  render() {
+    console.log(this.props.order + " ChildComponent Render");
+    /*接受一个从父元素传来的props.order*/
+    return (
+      <button
+        onClick={() => {
+          this.setState({ isUpdate: true });
+        }}
+      >
+        {this.props.order + " button updated? : " + this.state.isUpdate}
+        <br />
+      </button>
+    );
+  }
 }
 
-export default App;
+//父组件
+class SuperComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChanged: "false"
+    };
+  }
+
+  UNSAFE_componentWillUpdate() {
+    console.log("SuperComponent WillUpdate");
+  }
+  componentDidUpdate() {
+    console.log("SuperComponent DidUpdate");
+  }
+  UNSAFE_componentWillMount(){
+    console.log("SuperComponent WillMounted");
+  }
+  componentDidMount() {
+    console.log("SuperComponent Mounted");
+  }
+
+  render() {
+    //渲染三个子组建，并提供一个按钮，当按钮被点击时强行update
+    console.log("SuperComponent Render");
+    return (
+      <div>
+        <SubComp order="1st" />
+        <br />
+        <SubComp order="2nd" />
+        <br />
+        <SubComp order="3rd" />
+        <br />
+        <button
+          onClick={() => {
+            this.setState({
+              isChanged: "true"
+            });
+          }}
+        >
+          Change Sup Data
+        </button>
+        <div>{this.state.isChanged}</div>
+      </div>
+    );
+  }
+}
+
+export default SuperComp;
